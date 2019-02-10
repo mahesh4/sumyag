fba7042603d21e4d1032ddb225dfc01f9fe87271
magesh <magesh22111995@gmail.com>
2019-02-10 10:40:23 +0530

    added the submit button

--- scripts/app.js
+++ scripts/app.js
@@ -27,20 +27,22 @@ $(document).ready(function(){
         "data": "Sumyag"
     }];
 
-    alert(respose[0]);
 
     for (let i=0; i<response.length; i++) {
-    let fieldName = respose[i]['field'];
+    let fieldName = response[i]['field'];
     let fieldData = response[i]['data'];
     htmlCode += `<div class=form-group row> 
                     <label for="field-${i}" class="col-sm-2 col-form-label">${fieldName}</label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="field-${i}" value="${fieldData}">
                     </div>
-                </div>`
+                 </div>
+                 <br> <br> <br>`
     };
 
-    alert(htmlCode);
-
-      $("form").append(htmlCode);
+    htmlCode+= `<center> 
+                <button type="submit" class="btn btn-primary">Submit</button> 
+                <button type="button" class="btn btn-danger">Cancel</button>
+                <center>`;
+    $("form").append(htmlCode);
 });

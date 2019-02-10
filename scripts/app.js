99c2af10b2bfff5f32cd41d939ffdbfe82c60f32
magesh <magesh22111995@gmail.com>
2019-02-10 13:11:01 +0530

    added categorization select button

--- scripts/app.js
+++ scripts/app.js
@@ -33,16 +33,47 @@ $(document).ready(function(){
     let fieldData = response[i]['data'];
     htmlCode += `<div class=form-group row> 
                     <label for="field-${i}" class="col-sm-2 col-form-label">${fieldName}</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="field-${i}" value="${fieldData}">
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="field-${i}" name="${fieldName}" value="${fieldData}">
                     </div>
-                 </div>
+                    <div class="col-sm-2">
+                        <div class="form-group">
+                        <select class="form-control" id="fieldCategory-${i}" name="${fieldName}_category">
+                            <option hidden> Type </option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </div>
+                    </div>
+                </div>
                  <br> <br> <br>`
     };
 
-    htmlCode+= `<center> 
+    htmlCode += `<center> 
                 <button type="submit" class="btn btn-primary">Submit</button> 
                 <button type="button" class="btn btn-danger">Cancel</button>
                 <center>`;
-    $("form").append(htmlCode);
+    $( "form" ).append( htmlCode );
+
+    $( "#ResponseForm" ).submit( function() {
+        console.log( $( this ).serializeArray() );
+        alert( JSON.stringify( $( this ).serializeArray() ) );
+            
+        // $.ajax({
+        //     type: "POST",
+        //     url: url,
+        //     data: $( this ).serializeArray(),
+        //     success: function( data ) {
+        //         respose = JSON.parse(data);
+        //         console.log('data received from the server');
+        //     },
+        //     fail: function(xhr, textStatus, errorThrown){
+        //          alert('request failed');
+        //     },
+        //     dataType: JSON
+        //   });        
+    })
+
 });
